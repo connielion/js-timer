226f6be542b0f8be681e54c8943bdac9f54e4250
Connie <clai011@ucr.edu>
2018-10-14 02:19:32 -0700

    cny and mdb added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let timeRightNow = new Date();
+document.getElementById("timeRightNow").innerHTML = timeRightNow;
+
 // Set date we are counting down to
 let timeUntilChristmas = new Date("Dec 25, 2018, 00:00:00").getTime();
 
@@ -20,8 +23,8 @@ let i = setInterval(
     // Output days
     document.getElementById("daysLeft").innerHTML = days + " days";
     document.getElementById("hoursLeft").innerHTML = hours + " hours ";
-    document.getElementById("minutesLeft").innerHTML = minutes + " minutes ";
-    document.getElementById("secondsLeft").innerHTML = seconds + " seconds ";
+    document.getElementById("minutesLeft").innerHTML = minutes + " minutes and";
+    document.getElementById("secondsLeft").innerHTML = seconds + " seconds left.";
 
 
     if ( timeLeft< 0 ) {
@@ -29,30 +32,63 @@ let i = setInterval(
       document.getElementById("daysLeft").innerHTML = "Happy Holidays!";
     }
   }, 1000);
+//
 
 // Chinese New Year
 
-let timeTilCNY = new Date("Feb 5, 2018, 00:00:00").getTime();
+let cny = new Date("Feb 05, 2019, 00:00:00").getTime();
 
 let c = setInterval(
-    function() {
-      let currentDate = new Date().getTime();
+  function() {
+    let currentTime = new Date().getTime();
 
-      let untilCNY = timeTilCNY - currentDate;
-      // Calculate days, hours, minutes, and seconds left
-      let daysLeftCNY = Math.floor(timeTilCNY /(1000 * 60 * 60 * 24));
+    let timeLeftUntilCNY = cny - currentTime;
 
-      let hoursLeftCNY = Math.floor((timeTilCNY % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let d = Math.floor(timeLeftUntilCNY/(1000 * 60 * 60 * 24));
 
-      let minutesLeftCNY = Math.floor((timeTilCNY %(100 * 60 * 60))/(100 * 60));
+    let hr = Math.floor((timeLeftUntilCNY % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-      let secondsLeftCNY = Math.floor((timeTilCNY % (1000 * 60 )) / 1000);
+    let min = Math.floor((timeLeftUntilCNY %(100 * 60 * 60))/(100 * 60));
 
-      // Output days
-      document.getElementById("daysLeftCNY").innerHTML = daysLeftCNY + " days";
-      document.getElementById("hoursLeftCNY").innerHTML = hoursLeftCNY + " hours ";
-      document.getElementById("minutesLeftCNY").innerHTML = minutesLeftCNY + " minutes ";
-      document.getElementById("secondsLeftCNY").innerHTML = secondsLeftCNY + " seconds ";
+    let sec = Math.floor((timeLeftUntilCNY % (1000 * 60 )) / 1000);
 
-    }, 1000
+    // Output days
+    document.getElementById("cnyDays").innerHTML = d + " days";
+    document.getElementById("cnyHours").innerHTML = hr + " hours ";
+    document.getElementById("cnyMinutes").innerHTML = min + " minutes and";
+    document.getElementById("cnySeconds").innerHTML = sec + " seconds left.";
+
+    if ( timeLeftUntilCNY < 0 ) {
+      clearInterval(c);
+      document.getElementById("cnyDays").innerHTML = "Happy Chinese New Year!";
+    }
+  }
+);
+
+
+// Countdown to Mom's Bday
+let halloween = new Date("Oct 31, 2018, 00:00:00").getTime();
+
+let b = setInterval(
+  function() {
+    let n = new Date().getTime();
+    let timeTilMBD = halloween - n;
+
+    let _day = Math.floor(timeTilMBD/(1000 * 60 * 60 * 24));
+
+    let _hour = Math.floor((timeTilMBD % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    let _min = Math.floor((timeTilMBD %(100 * 60 * 60))/(100 * 60));
+
+    let _sec = Math.floor((timeTilMBD % (1000 * 60 )) / 1000);
+
+    // Output days
+
+    document.getElementById("day").innerHTML = _day + " days";
+    document.getElementById("hour").innerHTML = _hour + " hours";
+    document.getElementById("min").innerHTML = _min + " minutes and";
+    document.getElementById("sec").innerHTML = _sec + " seconds left";
+
+
+  }
 )
